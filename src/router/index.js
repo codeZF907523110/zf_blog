1701d421a6b5288ec9149090884da5e69d644ac1
流觞曲水 <907523110@qq.com>
2023-01-30 20:15:52 +0800

    更改目录

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-02-22 10:16:06
- * @LastEditTime: 2023-01-11 15:14:58
- * @LastEditors: zhangfeng16 zhangfeng16@shuidi-inc.com
+ * @LastEditTime: 2023-01-30 20:14:04
+ * @LastEditors: 流觞曲水 907523110@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /zf_blog/zfblog/src/router/index.js
  */
@@ -14,45 +14,45 @@ const routes = [
     },
     {
       path:'/display',
-      component: () => import('../pages/Display/Display.vue'),
+      component: () => import('@pages/Display/Display.vue'),
       children:[
         {
           path:'home',
-          component: () => import('../pages/Home/Home.vue')
+          component: () => import('@pages/Home/Home.vue')
         },
         {
           path:'techNology',
-          component: () => import('../pages/Technology/Technology.vue')
+          component: () => import('@pages/Technology/Technology.vue')
         },
         {
           path:'messageBoard',
-          component: () => import('../pages/MessageBoard/MessageBoard.vue')
+          component: () => import('@pages/MessageBoard/MessageBoard.vue')
         },
         {
           path:'articleContent',
-          component: () => import('../pages/ArticleContent/ArticleContent.vue')
+          component: () => import('@pages/ArticleContent/ArticleContent.vue')
         },
         {
           path:'myDiary',
-          component: () => import('../pages/MyDiary/MyDiary.vue')
+          component: () => import('@pages/MyDiary/MyDiary.vue')
         },
         {
           path:'myDiaryContent',
-          component: () => import('../pages/MyDiary/MyDiaryContent.vue')
+          component: () => import('@pages/MyDiary/MyDiaryContent.vue')
         }
       ]
     },
     {
       path:'/writeArticle',
-      component: () => import('../pages/WriteArticle/WriteArticle.vue')
+      component: () => import('@pages/WriteArticle/WriteArticle.vue')
     },
     {
       path:'/writeMarkdown',
-      component: () => import('../pages/WriteArticle/WriteMarkdown.vue')
+      component: () => import('@pages/WriteArticle/WriteMarkdown.vue')
     },
     {
       path:'/studyNotes',
-      component: () => import('../pages/StudyNotes/StudyNotes.vue')
+      component: () => import('@pages/StudyNotes/StudyNotes.vue')
     }
 ]
 const router = createRouter({
