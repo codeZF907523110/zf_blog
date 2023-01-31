d789f17115419f2412d748218eb6f5dc4fb8b4c0
流觞曲水 <907523110@qq.com>
2023-01-31 18:28:05 +0800

    修改域名

--- src/api/request.ts
+++ src/api/request.ts
@@ -3,7 +3,7 @@ import VueCookies from 'vue-cookies'
 import qs from 'qs'
 import { ElNotification } from 'element-plus'
 let config = {
-  baseURL: 'https://47.92.115.116:3006',
+  baseURL: 'http://zf-blog-serve.top',
   timeout: 120 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
   headers: {
