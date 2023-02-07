1de36d5c7998f9d964aefc4adb4e2eba9759f2ba
流觞曲水 <907523110@qq.com>
2023-02-07 14:12:55 +0800

    vercel跨域

--- src/api/proxy.ts
+++ src/api/proxy.ts
@@ -1,31 +1,21 @@
-/*
- * @Author: 流觞曲水 907523110@qq.com
- * @Date: 2023-02-07 14:01:57
- * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-07 14:03:23
- * @FilePath: /zf_blog/src/api/proxy.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-// 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-  let target = ''
-
-  // 代理目标地址
-  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/api')) {
-  // 这里填目标地址
-    target = 'https://zf-blog-serve.top'
-  }
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/api/': '/api'
+    let target = ''
+    // 代理目标地址
+    // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+    // target 替换为你跨域请求的服务器 如： http://baidu.com
+    if (req.url.startsWith('/api')) {
+        target = 'https://zf-blog-serve.top'
     }
-  })(req, res)
+    // 创建代理对象并转发请求
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+      pathRewrite: {
+          // 通过路径重写，去除请求路径中的 `/backend`
+          // 例如 /backend/user/login 将被转发到 https://fanyi-api.baidu.com/user/login
+          '^/api/': '/api',
+      },
+    })(req, res)
 }
--- src/api/request.ts
+++ src/api/request.ts
@@ -3,7 +3,7 @@ import VueCookies from 'vue-cookies'
 import qs from 'qs'
 import { ElNotification } from 'element-plus'
 let config = {
-  baseURL: '/api',
+  baseURL: '',
   timeout: 120 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
   headers: {
