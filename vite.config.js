db5d1025af1a7e175a96b19fb76cb4e142d326a6
zhangfeng16 <907523110@qq.com>
2023-04-28 16:12:12 +0800

    修改端口

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-20 14:54:58
  * @LastEditors: zhangfeng16 907523110@qq.com
- * @LastEditTime: 2023-03-16 17:24:08
+ * @LastEditTime: 2023-04-28 16:11:07
  * @FilePath: /zf-blog/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -52,7 +52,7 @@ export default ({ mode }) => {
       },
     },
     server: {
-      port: 5000,
+      port: 80,
       host: '0.0.0.0',
       https: false,
       hmr: true,
