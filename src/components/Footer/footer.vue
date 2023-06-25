6f57ceb473ab8d28bb9cd0ff2e4db65c2b968f40
zhangfeng16 <907523110@qq.com>
2023-06-25 16:24:43 +0800

    添加备案号

--- src/components/Footer/footer.vue
+++ src/components/Footer/footer.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: 流觞曲水 907523110@qq.com
  * @Date: 2023-01-23 13:40:26
- * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-01-30 13:06:30
+ * @LastEditors: zhangfeng16 907523110@qq.com
+ * @LastEditTime: 2023-06-25 16:23:09
  * @FilePath: /zf-blog/src/components/Footer/footer.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div class='footer'>
-        
+      <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2023011707号-1</a>
     </div>
 </template>
 
@@ -17,10 +17,13 @@
 
 <style lang='scss'>
   .footer{
-      width: 100%;
-      height: 240px;
-      background: rgb(48, 49, 51);
-      margin-top: 20px;
-      
+    width: 100%;
+    height: 240px;
+    background: rgb(48, 49, 51);
+    margin-top: 20px;
+    padding: 100px;
+    a {
+      color: white;
+    }
   }
 </style>
